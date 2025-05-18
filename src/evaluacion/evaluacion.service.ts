6a6aee5486b3bb3dc41fa8adc3723f28fed63f59
Joan Amaya <j.amayab@uniandes.edu.co>
2025-05-18 02:38:16 -0500

    Controladores finalizados

--- src/evaluacion/evaluacion.service.ts
+++ src/evaluacion/evaluacion.service.ts
@@ -2,21 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EvaluacionEntity } from './evaluacion.entity/evaluacion.entity';
 import { Repository } from 'typeorm';
-import { BusinessError, BusinessLogicException } from 'src/shared/errors/business-errors';
+import {
+  BusinessError,
+  BusinessLogicException,
+} from 'src/shared/errors/business-errors';
 
 @Injectable()
 export class EvaluacionService {
-    constructor(
-            @InjectRepository(EvaluacionEntity)
-            private readonly evaluacionRepository: Repository<EvaluacionEntity>,
-        ) {}
-        async crearEvaluación(evaluacion: EvaluacionEntity): Promise<EvaluacionEntity> {
-            if (evaluacion.profesor.cedula!= evaluacion.proyecto.estudiante.cedula && evaluacion.proyecto.notaFinal>=0 && evaluacion.proyecto.notaFinal<=5)
-              return await this.evaluacionRepository.save(evaluacion);
-            else
-              throw new BusinessLogicException(
-                'Error precondicion',
-                BusinessError.PRECONDITION_FAILED,
-              );
-          }
+  constructor(
+    @InjectRepository(EvaluacionEntity)
+    private readonly evaluacionRepository: Repository<EvaluacionEntity>,
+  ) {}
+  async crearEvaluación(
+    evaluacion: EvaluacionEntity,
+  ): Promise<EvaluacionEntity> {
+    if (
+      evaluacion.profesor.cedula != evaluacion.proyecto.estudiante.cedula &&
+      evaluacion.proyecto.notaFinal >= 0 &&
+      evaluacion.proyecto.notaFinal <= 5
+    )
+      return await this.evaluacionRepository.save(evaluacion);
+    else
+      throw new BusinessLogicException(
+        'Error precondicion',
+        BusinessError.PRECONDITION_FAILED,
+      );
+  }
 }
